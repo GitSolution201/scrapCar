717d726de302ed941c286bcd9b4d9cc846b93f0f
Ali Akber <alisher6269@gmail.com>
2025-06-11 01:37:51 +0500

    add pkgs and suplash screen sound done

--- scr/Screens/Splash/splash.tsx
+++ scr/Screens/Splash/splash.tsx
@@ -4,25 +4,50 @@ import {wp} from '../../Helper/Responsive';
 import {useSelector} from 'react-redux';
 import {navigationRef} from '../../navigationRef';
 import {CommonActions} from '@react-navigation/native';
+import Sound from 'react-native-sound';
 
 const Splash = () => {
   const token = useSelector((state: any) => state.auth.token);
 
   useEffect(() => {
-    const timeout = setTimeout(() => {
-      if (navigationRef.isReady()) {
-        const targetScreen = token ? 'MainStack' : 'AuthStack';
+    // Allow playback
+    Sound.setCategory('Playback');
 
-        navigationRef.dispatch(
-          CommonActions.reset({
-            index: 0,
-            routes: [{name: targetScreen}],
-          }),
-        );
-      }
-    }, 2000);
+    const splashSound = new Sound(
+      require('../../../assets/sounds/insta_startup.mp3'),
+      error => {
+        if (error) {
+          console.log('Sound loading failed', error);
+          return;
+        }
 
-    return () => clearTimeout(timeout);
+        // Play the sound
+        splashSound.play(success => {
+          if (success) {
+            console.log('Sound played successfully');
+          } else {
+            console.log('Sound playback failed');
+          }
+
+          // Navigate after playback
+          if (navigationRef.isReady()) {
+            const targetScreen = token ? 'MainStack' : 'AuthStack';
+
+            navigationRef.dispatch(
+              CommonActions.reset({
+                index: 0,
+                routes: [{name: targetScreen}],
+              }),
+            );
+          }
+        });
+      },
+    );
+
+    // Cleanup
+    return () => {
+      splashSound.release();
+    };
   }, [token]);
 
   return (
