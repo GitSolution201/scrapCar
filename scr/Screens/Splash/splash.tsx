da6146a5f5ac0144418b6083535bf5cb6a9feca0
Ali Akber <alisher6269@gmail.com>
2025-06-12 12:58:22 +0500

    Set sound

--- scr/Screens/Splash/splash.tsx
+++ scr/Screens/Splash/splash.tsx
@@ -1,5 +1,5 @@
 import {Image, SafeAreaView, StyleSheet} from 'react-native';
-import React, {useEffect} from 'react';
+import React, {useEffect, useRef} from 'react';
 import {wp} from '../../Helper/Responsive';
 import {useSelector} from 'react-redux';
 import {navigationRef} from '../../navigationRef';
@@ -8,47 +8,51 @@ import Sound from 'react-native-sound';
 
 const Splash = () => {
   const token = useSelector((state: any) => state.auth.token);
+  const splashSoundRef = useRef<Sound | null>(null); // useRef to persist sound instance
 
   useEffect(() => {
-    // Allow playback
     Sound.setCategory('Playback');
 
-    const splashSound = new Sound(
+    // Preload sound immediately on mount
+    splashSoundRef.current = new Sound(
       require('../../../assets/sounds/insta_startup.mp3'),
       error => {
         if (error) {
           console.log('Sound loading failed', error);
+          navigate();
           return;
         }
 
-        // Play the sound
-        splashSound.play(success => {
+        // Immediately play after load
+        splashSoundRef.current?.play(success => {
           if (success) {
             console.log('Sound played successfully');
           } else {
             console.log('Sound playback failed');
           }
 
-          // Navigate after playback
-          if (navigationRef.isReady()) {
-            const targetScreen = token ? 'MainStack' : 'AuthStack';
-
-            navigationRef.dispatch(
-              CommonActions.reset({
-                index: 0,
-                routes: [{name: targetScreen}],
-              }),
-            );
-          }
+          navigate();
         });
       },
     );
 
-    // Cleanup
     return () => {
-      splashSound.release();
+      splashSoundRef.current?.release();
     };
   }, [token]);
+
+  const navigate = () => {
+    if (navigationRef.isReady()) {
+      const targetScreen = token ? 'MainStack' : 'AuthStack';
+
+      navigationRef.dispatch(
+        CommonActions.reset({
+          index: 0,
+          routes: [{name: targetScreen}],
+        }),
+      );
+    }
+  };
 
   return (
     <SafeAreaView style={styles.container}>
